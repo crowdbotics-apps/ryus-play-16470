927dc5b06d84cc29ff55ab56629110376f6dc87f
Crowdbotics <team@crowdbotics.com>
2020-05-01 00:18:50 +0000

    Initial commit.

---
+++ backend/ryus_play_16470/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ryus_play_16470 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ryus_play_16470.settings')
+
+application = get_wsgi_application()
